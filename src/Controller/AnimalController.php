90d077dfdb9bb26bd77909d584d40d0ea467080a
Irene <ireneog_72@hotmail.es>
2019-12-03 10:58:38 +0100

    Use doctrine find method

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -18,7 +18,7 @@ class AnimalController extends AbstractController
 
     public function save()
     {
-        //return new Response('Hola mundillo');
+        //return new Response('Hola desde la acción save');
 
         //**************************************************** */
         //Guardar en una tabla de la bd
@@ -39,5 +39,25 @@ class AnimalController extends AbstractController
         $entityManager->flush();
 
         return new Response('El animal guardado tiene el id: ' . $animal->getId());
+    }
+
+    public function animal( $id )
+    {
+        //return new Response('Hola desde la acción animal');
+
+        //1. Cargar repositorio. Me permite traer todos los métodos de consulta que tiene nuestro modelo por defecto + los que hayamos añadido si le configuramos un repositorio
+        $animal_repo = $this->getDoctrine()->getRepository(Animal::class);
+
+        //2. Consulta
+        $animal = $animal_repo->find( $id );
+
+        //3. Comprobar si el resultado es correcto
+        if( !$animal ){
+            $message = 'El animal no existe';
+        }else{
+            $message = 'Tu animal elegido es: ' . $animal->getTipo() .  ' - ' . $animal->getRaza();
+        }
+
+        return new Response( $message );
     }
 }
